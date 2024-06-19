9f430d94a6e946bfea5f89f10eb67e70baca9259
joan.peruchet <joan17cast@gmail.com>
2024-06-19 08:43:48 +0200

    Add icons file

---
+++ src/utils/icons.tsx
@@ -0,0 +1,17 @@
+export const icons = {
+  arrowRight: (
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="css-i6dzq1"
+    >
+      <polyline points="9 18 15 12 9 6"></polyline>
+    </svg>
+  ),
+};
