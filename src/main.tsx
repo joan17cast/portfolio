e4653f9a11cb3e5a0aca119cf664016921de8841
joan.peruchet <joan17cast@gmail.com>
2024-06-19 16:42:10 +0200

    Update i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import HomePage from "./pages/home";
+
 import "./index.css";
 import "./i18n/i18n";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HomePage />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
