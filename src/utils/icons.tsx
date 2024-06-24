b134d0661b8e0df907a91c40f773794032ca489e
joan.peruchet <joan17cast@gmail.com>
2024-06-24 13:43:10 +0200

    Add new icons

--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -1,4 +1,34 @@
 export const icons = {
+  projects: (
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="16 18 22 12 16 6"></polyline>
+      <polyline points="8 6 2 12 8 18"></polyline>
+    </svg>
+  ),
+  contacts: (
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+      <circle cx="12" cy="7" r="4"></circle>
+    </svg>
+  ),
   download: (
     <svg
       viewBox="0 0 24 24"
