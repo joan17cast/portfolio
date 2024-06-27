f60f6ff9fc20065c1b5d15933d821d8a0096d2b4
joan.peruchet <joan17cast@gmail.com>
2024-06-27 11:10:52 +0200

    Update static content

--- src/utils/staticContent.ts
+++ src/utils/staticContent.ts
@@ -1,29 +1,75 @@
+import bonareaImg from "@/assets/images/brands/bonarea.png";
+import dammImg from "@/assets/images/brands/damm.png";
+import freseniusImg from "@/assets/images/brands/fresenius.png";
+import grifolsImg from "@/assets/images/brands/grifols.png";
+import puigImg from "@/assets/images/brands/puig.png";
+import rdtImg from "@/assets/images/brands/rdt.png";
+import trgImg from "@/assets/images/brands/trg.png";
+
 export const iconStack = [
-    "typescript",
-    "javascript",
-    "tailwindcss",
-    "react",
-    "flutter",
-    "html5",
-    "css3",
-    "nodedotjs",
-    "nextdotjs",
-    "jest",
-    "docker",
-    "git",
-    "slack",
-    "github",
-    "redux",
-    "reactquery",
-    "trpc",
-    "zod",
-    "playwright",
-    "axios",
-    "linear",
-    "vite",
-    "gitlab",
-    "visualstudiocode",
-    "androidstudio",
-    "sonarqube",
-    "figma",
-  ];
+  "typescript",
+  "javascript",
+  "tailwindcss",
+  "react",
+  "flutter",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "nextdotjs",
+  "jest",
+  "docker",
+  "git",
+  "slack",
+  "github",
+  "redux",
+  "reactquery",
+  "trpc",
+  "zod",
+  "playwright",
+  "axios",
+  "linear",
+  "vite",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+export const companysData = [
+  {
+    icon: grifolsImg,
+    url: "https://www.grifols.com/",
+    alt: "Grifols",
+  },
+  {
+    icon: rdtImg,
+    url: "https://www.tubosreunidosgroup.com/rdt-usa/",
+    alt: "RDT",
+  },
+  {
+    icon: dammImg,
+    url: "https://www.damm.com/",
+    alt: "Damm",
+  },
+  {
+    icon: bonareaImg,
+    url: "https://www.bonarea.com/",
+    alt: "BonÀrea",
+  },
+  {
+    icon: freseniusImg,
+    url: "https://www.freseniusmedicalcare.es/",
+    alt: "Fresenius",
+  },
+  {
+    icon: trgImg,
+    url: "https://www.tubosreunidosgroup.com/",
+    alt: "TRG",
+  },
+  {
+    icon: puigImg,
+    url: "https://www.puig.com/",
+    alt: "Puig",
+  }
+];
