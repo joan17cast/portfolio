7e72e608d0085f7483c63649efa826666ac0215b
joan.peruchet <joan17cast@gmail.com>
2024-07-11 10:53:40 +0200

    Update tsconfig for json files

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from "react-i18next";
 import en from "./en.json";
 import es from "./es.json";
 
-i18n.use(initReactI18next).init({
+await i18n.use(initReactI18next).init({
   resources: {
     en: {
       translation: en,
