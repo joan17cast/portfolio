dcfbc275c5f33d0aac3694738743eb0ae09d839b
joan.peruchet <joan17cast@gmail.com>
2024-07-26 08:35:45 +0200

    Update navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,4 +1,3 @@
-import { icons } from "@/utils/icons";
 import { Link } from "@tanstack/react-router";
 
 const Navbar = () => {
@@ -6,7 +5,6 @@ const Navbar = () => {
     <div className="flex h-16 w-full flex-row items-center justify-center   bg-transparent p-4 shadow-md">
       <div className="flex w-[1000px] flex-row items-center justify-between gap-4 text-white">
         <Link to="/" className="flex flex-row items-center gap-4 text-xs">
-          {icons.home}
           <h1 className="text-lg font-semibold ">Joan Peruchet Castells</h1>
         </Link>
         <div className="font-thin text-gray-300">Español</div>
