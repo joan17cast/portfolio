e0d111b9c02c3f0acec7e9d85979145600147727
joan.peruchet <joan17cast@gmail.com>
2024-07-26 08:35:18 +0200

    Update not found page

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
+import notFoundImage from "@/assets/images/notFound.svg";
 
-const NotFound: React.FC = () => {
-  const { t } = useTranslation();
-
-  return <div className="p-4">TODO</div>;
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "radial-gradient(circle 1000px at top, rgba(120,119,198,0.3), rgba(20,20,20,0.3))",
+      }}
+      className="relative flex h-screen w-full flex-col items-center justify-start gap-4 overflow-auto bg-black p-4 pt-14 md:px-20"
+    >
+      <h1 className="">Pagina no encontrada</h1>
+      <img src={notFoundImage} className="h-96 w-96" />
+    </div>
+  );
 };
-
 export default NotFound;
