80619caeb2da557b494ebcd49e04fc0519e2220e
joan.peruchet <joan17cast@gmail.com>
2025-07-03 20:36:37 +0200

    Update styles from techCard

--- src/components/card/techCard.tsx
+++ src/components/card/techCard.tsx
@@ -9,9 +9,8 @@ const TechCard = ({ icon, label }: TechCardProps) => {
   return (
     <figure
       className={clsx(
-        "min-h-fit   transform cursor-pointer overflow-hidden rounded-2xl px-2",
-        "transition-all duration-200 ease-in-out hover:scale-[103%]",
-        "transform-gpu bg-transparent backdrop-blur-md [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+        "min-h-fit cursor-default overflow-hidden rounded-2xl px-2",
+        "bg-transparent backdrop-blur-md [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       )}
     >
       <div className="flex flex-col items-center justify-center gap-1 py-2 text-white text-sm">
